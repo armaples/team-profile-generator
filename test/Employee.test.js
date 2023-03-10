b9ea0831c0a2e226463115d65da2c3d69ec03a39
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-10 10:57:50 -0500

    Added employee tests

---
+++ test/Employee.test.js
@@ -0,0 +1,38 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee class', () => {
+    it('Should create new employee and give name, id, and email', () => {
+        const testingEmployeeName = "Alyssa";
+        const testingEmployeeId = 22;
+        const testingEmployeeEmail = "alyssa@email.com"
+        const employee = new Employee(testingEmployeeName, testingEmployeeId, testingEmployeeEmail);
+
+        expect(employee.name).toBe(testingEmployeeName);
+        expect(employee.id).toBe(testingEmployeeId);
+        expect(employee.email).toBe(testingEmployeeEmail);
+    });
+
+    it('getName() should return employee name', () => {
+        const employee = new Employee("Alyssa", 22, "alyssa@email.com");
+
+        expect(employee.getName()).toBe("Alyssa");
+    });
+
+    it('getId() should return employee id', () => {
+        const employee = new Employee("Alyssa", 22, "alyssa@email.com");
+
+        expect(employee.getId()).toBe(22);
+    });
+
+    it('getEmail() should return employee email', () => {
+        const employee = new Employee("Alyssa", 22, "alyssa@email.com");
+
+        expect(employee.getEmail()).toBe("alyssa@email.com");
+    });
+
+    it('getRole() should return Employee', () => {
+        const employee = new Employee();
+
+        expect(employee.getRole()).toBe("Employee");
+    });
+})
